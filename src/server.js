220dd8213dcd6b291f3c18456ee74129fe6f64cd
Tan Jia Xian <e0543981@u.nus.edu>
2021-07-26 20:08:58 +0800

    added secure cookie, sameSite="None" and trust proxy

--- src/server.js
+++ src/server.js
@@ -53,11 +53,13 @@ const sessionConfig = {
     collectionName: "sessions",
   }),
   cookie: {
-    secure: false,
+    secure: true,
     maxAge: 30 * 1000 * 60 * 60 * 24,
+    sameSite: "none",
   },
 };
 
+app.set("trust proxy", 1);
 app.use(session(sessionConfig));
 
 // Passport.js
